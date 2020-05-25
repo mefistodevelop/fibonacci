282045c9f845f25670d8743424653d780650d7eb
Oleg Rudenko <oleg.ru93@yandex.ru>
2020-05-25 19:38:06 +0300

    feat: add server api to get history of requests

--- src/server/index.js
+++ src/server/index.js
@@ -1,7 +1,36 @@
 const express = require('express');
+const cors = require('cors');
 
 const app = express();
 
+const database = [
+  {
+    id: 1,
+    ip: '122.123.1.1',
+    request: 10,
+    response: 300,
+    date: '11.11.2019',
+  },
+  {
+    id: 2,
+    ip: '122.124.1.2',
+    request: 1,
+    response: 123,
+    date: '01.01.2020',
+  },
+  {
+    id: 3,
+    ip: '122.129.5.1',
+    request: 1123,
+    response: 12300,
+    date: '07.03.2020',
+  },
+];
+
+app.get('/api/history', cors(), (req, res) => {
+  res.json(database);
+});
+
 app.use(express.static('dist'));
 app.get('/', (req, res) => res.send('Server is ready...'));
-app.listen(8080, () => console.log('Server has started. Open http://localhost:8080/'));
+app.listen(8080, () => console.log('Server has started on http://localhost:8080/'));
