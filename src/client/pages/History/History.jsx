efb2beb76fdf677f5116f8454de5537b70c26674
Oleg Rudenko <oleg.ru93@yandex.ru>
2020-05-25 19:41:11 +0300

    feat: History page should to get history from server

--- src/client/pages/History/History.jsx
+++ src/client/pages/History/History.jsx
@@ -1,13 +1,32 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './History.scss';
-import { HistoryTable } from './Table';
+import { HistoryTable } from './HistoryTable';
+import { fibApi } from '../../api/api';
+import { Spinner } from '../../components/Spinner/Spinner';
 
-const History = () => (
-  <div className="history">
-    <div className="history__container">
-      <HistoryTable />
+const History = () => {
+  const [history, sethistory] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const getHist = async () => {
+    setIsLoading(true);
+    const hist = await fibApi.getHistory();
+    setIsLoading(false);
+
+    sethistory(hist);
+  };
+
+  useEffect(() => {
+    getHist();
+  }, []);
+
+  return (
+    <div className="history">
+      <div className="history__container">
+        {isLoading ? <Spinner /> : <HistoryTable history={history} />}
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export { History };
--- src/client/pages/History/HistoryTable.jsx
+++ src/client/pages/History/HistoryTable.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { makeStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import Table from '@material-ui/core/Table';
@@ -9,47 +10,25 @@ import TableHead from '@material-ui/core/TableHead';
 import TablePagination from '@material-ui/core/TablePagination';
 import TableRow from '@material-ui/core/TableRow';
 
-const rows = [
-  {
-    id: 1,
-    ip: '122.123.1.1',
-    request: 10,
-    response: 300,
-    date: '11.11.2019',
-  },
-  {
-    id: 2,
-    ip: '122.124.1.2',
-    request: 1,
-    response: 123,
-    date: '01.01.2020',
-  },
-  {
-    id: 3,
-    ip: '122.129.5.1',
-    request: 1123,
-    response: 12300,
-    date: '07.03.2020',
-  },
-];
+const HistoryTable = ({ history }) => {
+  const rows = history;
 
-const columns = [
-  { id: 'ip', label: 'Ip address', minWidth: 100 },
-  { id: 'date', label: 'Date', minWidth: 100 },
-  { id: 'request', label: 'Request', minWidth: 100 },
-  { id: 'response', label: 'Response', minWidth: 100 },
-];
+  const columns = [
+    { id: 'ip', label: 'Ip address', minWidth: 100 },
+    { id: 'date', label: 'Date', minWidth: 100 },
+    { id: 'request', label: 'Request', minWidth: 100 },
+    { id: 'response', label: 'Response', minWidth: 100 },
+  ];
 
-const useStyles = makeStyles({
-  root: {
-    width: '100%',
-  },
-  container: {
-    maxHeight: 440,
-  },
-});
+  const useStyles = makeStyles({
+    root: {
+      width: '100%',
+    },
+    container: {
+      maxHeight: 440,
+    },
+  });
 
-export function HistoryTable() {
   const classes = useStyles();
   const [page, setPage] = React.useState(0);
   const [rowsPerPage, setRowsPerPage] = React.useState(10);
@@ -111,4 +90,14 @@ export function HistoryTable() {
       />
     </Paper>
   );
-}
+};
+
+HistoryTable.propTypes = {
+  history: PropTypes.arrayOf(PropTypes.object),
+};
+
+HistoryTable.defaultProps = {
+  history: [],
+};
+
+export { HistoryTable };
