165227f6e8e12c1b5ae532fd57b3647839471a0f
Oleg Rudenko <oleg.ru93@yandex.ru>
2020-06-15 10:58:36 +0300

    feat: add mysql database

--- src/server/index.js
+++ src/server/index.js
@@ -1,13 +1,62 @@
 const express = require('express');
 const cors = require('cors');
+const mysql = require('mysql');
 const { getHistory } = require('./api/history');
-const { getFibonacci } = require('./api/fibonacci');
+// const { getFibonacci } = require('./api/fibonacci');
+// const calcFibonacci = require('./fibonacci');
 
 const app = express();
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'm35j17n0D!',
+});
+
+
+const createTable = `
+CREATE TABLE IF NOT EXISTS history (
+  id SERIAL,
+  ip VARCHAR(45) NOT NULL,
+  date DATE,
+  request VARCHAR(255),
+  response VARCHAR(255),
+  PRIMARY KEY (id)
+);
+`;
+
+connection.connect((err) => {
+  if (err) throw err;
+  console.log('Database: Connection is open');
+});
+
+connection.query('CREATE DATABASE IF NOT EXISTS fib_db', (err) => {
+  if (err) throw err;
+  console.log('database is initialized');
+});
+
+connection.query('USE fib_db', (err) => {
+  if (err) throw err;
+});
+
+connection.query(createTable, (err) => {
+  if (err) throw err;
+  console.log('table is initialized');
+});
+
+
+connection.end((err) => {
+  if (err) throw err;
+  console.log('Database: Connection is closed');
+});
+
 
 app.get('/api/history', cors(), getHistory);
 
-app.get('/api/fib', cors(), getFibonacci);
+// app.get('/api/fib', cors(), (req, res) => {
+//   const { number } = req.query;
+//   const { ip } = req;
+//   calcFibonacci();
+// });
 
 app.use(express.static('dist'));
 app.get('/', (req, res) => res.send('Server is ready...'));
